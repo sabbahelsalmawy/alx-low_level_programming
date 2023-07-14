91d5823839f0730ec08eb8b729102f17b24f542d
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-13 18:22:05 -0700

    siz

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main: is start of program
+ * printf: to print output
+ * retuar: alwaes is (0)
+ */
+int main(void)
+{
+	printf("Size of a"sizeof(char)"\n");
+	printf("Size of a"sizeof(int)"\n");
+	printf("Size of a"sizeof(long)"\n");
+	printf("Size of a"sizeof(long long int)"\n");
+	printf("Size of a"sizeof(float)"\n");
+	return (0);
+}
