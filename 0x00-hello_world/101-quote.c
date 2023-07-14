f3d1cb8a441956978848b9f9484eaf9179a03195
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-13 19:07:29 -0700

    ww

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	print("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
 }
