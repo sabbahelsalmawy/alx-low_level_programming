a6c66fac40c53b5f2960d5cd58861b7e713ca8d5
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-14 19:06:23 -0700

    bb

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - is the function to start code entry point
+ *
+ *
+ * a:is the variablr that store number of ASCII vale of alpha
+ * putcher - is print the vale of a that have ASCII vale of alpha
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int a;
+
+	a = 97;
+	putchar(a);
+
+	return (0);
+}
