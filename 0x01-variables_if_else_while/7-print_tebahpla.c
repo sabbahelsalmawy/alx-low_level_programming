71ce549ddb1e6371ce5a2c73da40345551e25d99
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-14 19:49:41 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print the alphabet in reverse order in lowercase.
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	while (c = 'z')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+
+	return (0);
+}
