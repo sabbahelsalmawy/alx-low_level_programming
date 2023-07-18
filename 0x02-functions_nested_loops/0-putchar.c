edbf014f8e31c692d6f347e858d5a2241980eb2f
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-18 07:14:07 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main is start point
+ * printf to print on screen any word
+ * 
+ *
+ * return is always 0
+*/
+
+int main()
+{
+    printf("_putchar\n");
+    return 0;
+}
+
