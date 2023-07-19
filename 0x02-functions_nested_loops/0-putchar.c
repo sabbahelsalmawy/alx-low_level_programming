7caac903336eb0e98344a7a0bd0a59bd199286cb
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-18 18:45:46 -0700

    00

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - is start point in the program code
+ * main - is start point it has return valu 0
  *
  * printf - to print on screen any word easy
- *
- * Return of main - is always 0 to succsse if it 1 it will be error
+ * Return: 0 if the program is terminating correctly
+ * return: of main - is always ( 1 ) to succsse if it -1 it will be error
 */
 
 int main(void)
 {
 	printf("_putchar\n");
-		return (0);
+	return (0);
 }
 
