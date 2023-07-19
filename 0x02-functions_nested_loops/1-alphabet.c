4b56a77a481c48e64806017586becd1f44eee2e2
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-18 19:46:31 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,19 +3,19 @@
 /**
  * description: -  print alphabet lowercase by _putcher by used it two time
  *
- *
- * void print_alphabet(void): -  is function used to print alphabet
  * print_alphabet - prints the alphabe, in lowercase, followed by a new line
- *
-*/
+ */
 void print_alphabet(void)
 {
-	char al = 'a';
+	char al;
+
+	al = 'a';
 
 	while (al <= 'z')
 	{
 		_putchar(al);
 		al++;
 	}
-	_putchar('\n')
+
+	_putchar('\n');
 }
