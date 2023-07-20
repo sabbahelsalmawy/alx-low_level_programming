04685551af69b6ba0883e2a73514ccede2b459e0
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-19 22:14:16 -0700

    1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,13 +10,11 @@ int main(void)
 	int i;
 
 	i = 0;
-
 	printf("Infinite loop incoming :(\n");
 	while (i < 10)
 	{
 		putchar(i);
 	}
 	printf("Infinite loop avoided! \\o/\n");
-	
 	return (0);
 }
