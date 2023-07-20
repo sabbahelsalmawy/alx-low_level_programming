1fac3376aedab9902badcc8435840f60fba6edbb
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-19 17:21:43 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_abs - Check Main
+ * @r: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number r
+ */
+int _abs(int r)
+{
+	while (r > 0)
+	{
+		printf(r);
+		if (r < 0)
+			printf(r * (-1))
+	}
+}
