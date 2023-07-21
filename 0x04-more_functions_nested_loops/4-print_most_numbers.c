375f013b317a3beff88341e1aa845bbe08e7328c
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-20 19:29:00 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
+ */
+
+void print_most_numbers(void)
+{
+	int ch;
+
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		if (ch != '2' && ch != '4')
+			_putchar((num % 10) + '0');
+	}
+	_putchar('\n');
+}
