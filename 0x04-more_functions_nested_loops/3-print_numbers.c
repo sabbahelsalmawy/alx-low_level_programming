0431235795a234d2441b76c25c83b92ab8bb1bf8
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-20 19:05:58 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int a;
+
+	if (a <= '9' && a >= '0')
+	{
+		_putchar('%d');
+	}
+	_putchar('\n');
+}
