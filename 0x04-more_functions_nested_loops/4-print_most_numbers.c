240e923894923045d19b1c6832d60299a05abda4
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-20 19:37:08 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,9 @@ void print_most_numbers(void)
 {
 	int d;
 
-	for (d = '0'; d < '9'; d++)
+	for (d = 0; d < 9; d++)
 	{
-		if (d != '2' && d != '4')
+		if (d != 2 && d != 4)
 			_putchar((d % 10) + '0');
 	}
 	_putchar('\n');
