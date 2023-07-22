00b97c6fbe0870970a1f406434e802f8de9ddbb8
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-21 17:52:57 -0700

    99

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,16 +5,16 @@ int main (void)
 {
 	int a;
 
-	for (a = 0; a < 101; a++)
-		if (a % 3 = 0)
+	for (a = 1; a < 101; a++)
+		if (a % 3 == 0)
 		{
 			printf("Fizz");
 		}
-	if (a % 5 = 0)
+	if (a % 5 == 0)
 	{
 		printf("Buzz");
 	}
-	if (a % 3 = 0 && a % 5 = 0)
+	if (a % 3 == 0 && a % 5 == 0)
 	{
 		printf(FizzBuzz);
 	}
