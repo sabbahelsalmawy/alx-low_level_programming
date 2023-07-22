3e261abf29368fa39a56d16c91badf4804bfe09d
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-21 18:28:25 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
-/***/
 
-int main (void)
+/**
+* main - prints even numbers from 0 to 100
+* Return: 0
+*/
+
+int main(void)
 {
 	int a;
 
+	printf("%d", a);
 	for (a = 1; a < 101; a++)
-		if (a % 3 == 0)
+	{
+		if ((a % 3 == 0) && (a % 5 == 0))
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
-	if (a % 5 == 0)
-	{
-		printf("Buzz");
-	}
-	if (a % 3 == 0 && a % 5 == 0)
-	{
-		printf(FizzBuzz);
+		else if (a % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else
+		{
+			printf(" %d", a);
+		}
 	}
 	printf('\n');
+	return (0);
 }
