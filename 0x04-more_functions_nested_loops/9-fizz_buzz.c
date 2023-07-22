c697b9b6d905b11ee94aea231c6ce5a97eec8fa8
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-21 17:51:10 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/***/
+
+int main (void)
+{
+	int a;
+
+	for (a = 0; a < 101; a++)
+		if (a % 3 = 0)
+		{
+			printf("Fizz");
+		}
+	if (a % 5 = 0)
+	{
+		printf("Buzz");
+	}
+	if (a % 3 = 0 && a % 5 = 0)
+	{
+		printf(FizzBuzz);
+	}
+	printf('\n');
+}
