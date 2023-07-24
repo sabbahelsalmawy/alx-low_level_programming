dfea1193764e83eed564ed24a627af5e118b3e10
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-24 16:22:08 -0700

    1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,8 +3,11 @@
 /**
  * that function is prototybe
  * @a = b and @b = a
- * swap int vale to another
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
  */
+
 
 void swap_int(int *a, int *b)
 {
