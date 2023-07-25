4b19852f7574edac05c873ac1063390cc9c1e2a4
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-24 17:56:35 -0700

    6

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include"main"
+ /**
+  * puts - prints a string, followed by a new line,
+  * @str: pointer to the string to print
+  * Return: void
+*/
+
+void _puts(char *str)
+{
+	for (;*str != '\0'; str++)
+		_putchar(str + '0');
+	_putchar('\n');
+}
