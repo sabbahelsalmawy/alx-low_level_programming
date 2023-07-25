43862a898a197e2077f695aa5f447570619d6465
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-24 17:08:19 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * strlen - is function calclate the returns the length of a string.
+ * @s is pointer string value
+ * @s is character 
+ */
+
+int _strlen(char *s);
+{
+	int x = 0;
+
+	for (;x = 0; *s != '0'; x++)
+		return (0);
+}
