da302a3476158adc0874a69d498d9b9bf2a349c6
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-24 18:10:30 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+  * _puts - Prints a string
+  * @str: The string to print
+  *
+  * Return: void
+  */
+
+void print_rev(char *s)
+{
+        for (; *str != '\0'; str--)
+                _putchar(*str + 0);
+        _putchar('\n');
+}
