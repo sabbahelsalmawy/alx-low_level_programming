af84f915938349f48184e9e568e26613a4c08c3f
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-27 16:55:54 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int x, z;
+	
+	x = 0;
+	z = 0;
+	while(dest[x] != '\0')
+		x++;
+	for(;src[z]; z++)
+	{
+		dest[x] = src[z];
+			x++;
+	}
+	return(dest);
+}
