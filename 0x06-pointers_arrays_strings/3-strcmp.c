7953c73f6a9931219be7e99aa69383395a668246
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-27 18:02:44 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
+ *
+ * Return: s1[i] - s2[i]
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int c;
+
+	c = 0;
+	while (s1[c] != '\0' && s2[c] != '\0')
+	{
+		if (s1[c] != s2[c])
+			return (s1[c] - s2[c]);
+	s1[c]++;
+	s2[c]++;
+	}
+	return (0);
+}
