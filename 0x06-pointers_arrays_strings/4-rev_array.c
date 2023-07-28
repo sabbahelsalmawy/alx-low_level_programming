58e766bbc4a419962450ad833608fd305d0be477
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-27 18:43:31 -0700

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int x, y;
+
+	x = 0;
+	y = 0;
+	while (x < n)
+	{
+		x++;
+		n--;
+		y = a[x];
+		a[x] = a[n];
+		a[n] = y;
+	}
+}
