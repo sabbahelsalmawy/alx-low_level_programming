ab6f3316bd6560ee67b8dcba4447c8397743b7ca
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-27 19:20:32 -0700

    45

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,12 +7,12 @@
  *
  * Return: up
  */
-char *string_toupper(char *)
+char *string_toupper(char *up)
 {
 	int i;
 
 	i = 0;
-	while (up[i] != '\0')
+	for (; up[i] != '\0';)
 	{
 		if (up[i] >= 'a' && up[i] <= 'z')
 			up[i] = up[i] - 32;
