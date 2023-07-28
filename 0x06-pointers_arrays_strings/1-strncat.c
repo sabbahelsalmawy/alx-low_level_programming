9ab08babe8f1e00061d3c4c8deb8da42b247a85b
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-27 17:18:01 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,9 +11,9 @@ char *_strncat(char *dest, char *src, int n)
 	
 	x = 0;
 	z = 0;
-	while (dest[x] != n)
+	while (dest[x] != '\0')
 		x++;
-	for (; src[z]; z++)
+	for (; z < n && src[z] != '\0'; z++)
 	{
 		dest[x] = src[z];
 		x++;
