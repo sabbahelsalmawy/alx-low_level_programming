bc38eb57361de27b505ee76a1368fd4f5560065f
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-27 17:00:39 -0700

    00

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,22 +3,21 @@
  * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
- *
  * Return: void
  */
 
 char *_strcat(char *dest, char *src)
 {
 	int x, z;
-	
+
 	x = 0;
 	z = 0;
-	while(dest[x] != '\0')
+	while (dest[x] != '\0')
 		x++;
-	for(;src[z]; z++)
+	for (; src[z]; z++)
 	{
 		dest[x] = src[z];
 			x++;
 	}
-	return(dest);
+	return (dest);
 }
