135fb8339f1ddd486d8ba26b57c0c0cc4460bcdc
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-07-28 17:55:55 -0700

    7

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
  *
  * Return: A pointer to the changed string.
  */
-char *cap_string(char *s)
+char *cap_string(char *)
 {
 	int a = 0, i;
 	int cspc = 13;
