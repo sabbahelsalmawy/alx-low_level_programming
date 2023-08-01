349f9a50a94b31d18a6c4f93a3d60439c57c583a
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-08-01 16:50:40 -0700

    1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+  * _memcpy - copy memory area
+  * @dest: dest memory area
+  * @src: source memory area
+  * @n: bytes from memory area to copy
+  *
+  * Return: memory area replaced
+  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x;
+
+	x = 0;
+	for (; x < n; x++)
+		dest[x] = src;
+
+	return (dest);
+}
+
