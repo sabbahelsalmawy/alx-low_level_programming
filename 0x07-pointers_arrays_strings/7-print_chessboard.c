f7fd12dd4fc5f6fbcdcb87384dcb68b91fb6161c
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-08-01 19:12:15 -0700

    6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: input pointer.
+ * Return: no return.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int b, c;
+
+	b = 0;
+	c = 0;
+	for (; b < 8; b++)
+	{
+		for (; c < 8; c++)
+		{
+			_putchar(a[b][c]);
+		}
+
+		_putchar('\n');
+	}
+}
