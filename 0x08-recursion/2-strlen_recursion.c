6e327059668ffecc6c2af0e45abf4f155442babb
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-08-02 19:30:18 -0700

    22222

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,11 +7,9 @@
 
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
-		_strlen_recursion(s + 1);
-	else
+	if (*s == '\0')
 		return (0);
-	return (1 + _strlen_recursion(s + 1));
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
-
 
