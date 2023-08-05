4461baa2c618f20f16a7dd47601372176303d7bf
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-08-05 16:43:23 -0700

    33

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,17 +8,13 @@
   * Return: Always zero
   */
 
-
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
 		printf("Error\n");
-                return (1);
+		return (1);
 	}
-	else
-	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-		return (0);
-	}
-
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
