db5ea27883d9ec0378010e17e98ca10e121f49dc
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-08-05 19:38:01 -0700

    100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,32 +7,32 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	if (argc == 2)
-	{
-		int x, y = 0, z = atoi(argv[1]);
-		int a = [25,10,5,2,1];
+	int cents, ncoins = 0;
 
-		for (x = 0; x < 5; x++)
-		{
-			if (z >= a[x])
-			{
-				y += z / a[x];
-				z = z % a[x];
-				if (z % a[x] == 0)
-				{
-					break;
-				}
-			}
-		}
-		printf("%d\n", y);
-	}
-	else
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
+	}
+	printf("%d\n", ncoins);
 	return (0);
 }
