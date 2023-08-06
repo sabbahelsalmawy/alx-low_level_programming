282c78c033116cec90ad3ad2f6fab71c061451ab
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-08-05 17:53:57 -0700

    4444

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,34 +11,23 @@
 int main(int argc, char *argv[])
 {
 	int x;
-	int z, sum = 0;
 	char *e;
 
 	if (argc > 1)
 	{
-		for (x = 1; x < argc; x++)
-		{
-			e = argv[x];
-
-			for (z = 0; z < strlen(e); z++)
+		for (e = argv[argc]; *e; e++)
+			if (*e < '0' || *e > '9')
 			{
-				if (e[z] < '9' || e[z] > '0')
-				{
-					printf("Error\n");
-					return (1);
-				}
+				printf("Error\n");
+				return (1);
 			}
-
-			sum += atoi(e);
-			e++;
-		}
-
+		sum += atoi(e);
+		e++;
 		printf("%d\n", sum);
 	}
-	else
-	{
-		printf("0\n");
-	}
-
-	return (0);
+			else
+			{
+				printf("0\n");
+			}
+		return (0);
 }
