1d5758bfa9517f0aaf809cbf35522572f6dc6134
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-08-14 19:31:38 -0700

    4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 /**
  * struct dog - struct that stores some information of a dog
  * @name: name of the dog
@@ -18,5 +25,8 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
