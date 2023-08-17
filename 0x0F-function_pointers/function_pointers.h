848b7e763cd7fe698d72c3834a3db44af47c8a79
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-08-16 17:27:13 -0700

    0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 #include<stdlib.h>
-
+#include<stdio.h>
 int (*get_op_func(char *s))(int, int);
 int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
