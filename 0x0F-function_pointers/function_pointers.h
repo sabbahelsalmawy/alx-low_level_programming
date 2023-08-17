1b51cabc83f2d10817ce1be566793f442a0ebfd6
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-08-16 17:19:37 -0700

    0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,26 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include<stdlib.h>
+#include<stdio.h>
+
+int (*get_op_func(char *s))(int, int);
+int int_index(int *array, int size, int (*cmp)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
+void print_name(char *name, void (*f)(char *));
+
+
+int _putchar(char x);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+
+#endif
