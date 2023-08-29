a2c6ec3645d0ba2e63484d4dc8276d89735eea69
sabbahelsalmawy <sabbaheldinelsalmawy@gmail.com>
2023-08-28 18:42:14 -0700

    999

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,24 +8,38 @@
  *
  * Return: the address of the new node
  */
+
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @idx: index of the list where the new node is
+ * added.
+ * @n: integer element.
+ *
+ * Return: the address of the new node, or NULL if it
+ * failed.
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int x;
 	listint_t *nw;
-	listint_t *z;
+	listint_t *h;
 
 	h = *head;
-
+	
+	if (h == NULL && idx != 0)
+		return (NULL);
 	if (idx != 0)
 	{
-		for (x = 0; x < idx - 1 && x != NULL; x++)
+		for (x = 0; x < idx - 1 && h != NULL; x++)
 		{
 			h = h->next;
 		}
 	}
 
-	if (z == NULL && idx != 0)
-		return (NULL);
 
 	nw = malloc(sizeof(listint_t));
 	if (nw == NULL)
@@ -40,8 +54,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	else
 	{
-		nw->next = z->next;
-		z->next = nw;
+		nw->next = h->next;
+		h->next = nw;
 	}
 
 	return (nw);
